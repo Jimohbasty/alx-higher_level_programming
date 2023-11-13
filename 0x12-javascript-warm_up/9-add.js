aa4001d78a09336050966a7888c7c37a3d3dbfee
jimohbasty <jimohabdulbasit@gmail.com>
2023-11-13 18:44:35 +0100

    java

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
 
-const add = (a, b) => {
-    console.log(a + b);
-};
-
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-add(a, b);
+add(Number(process.argv[2]), Number(process.argv[3]));
